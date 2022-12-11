66fd486bd59c03457d158a73adf165c762891904
Warwick Molloy <wmolloy@gmail.com>
2022-12-11 16:49:51 +1100

    Generates first test outcome

--- cfgen/cfgen/Program.cs
+++ cfgen/cfgen/Program.cs
@@ -1,13 +1,16 @@
 ﻿using Optional;
 
 using kondensor.cfgenlib;
-
+using kondensor.cfgenlib.writer;
 
 public class Program
 {
   public static void Main(string[] args)
   {
-    TemplateDocument template = new TemplateDocument(Option.Some("Application VPC"));
-    Console.WriteLine("Hello, World!");
+    TemplateDocument template = new TemplateDocument(new Header("Test template"));
+    
+    YamlWriter writer = new YamlWriter();
+
+    writer.WriteFile(writeFileName:"test.yaml", template);
   }
 }
