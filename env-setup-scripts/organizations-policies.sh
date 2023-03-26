0b2d9c911432f6eef30d4f1e86aed7ac1b8443f2
Warwick Molloy <wmolloy@gmail.com>
2023-03-26 14:13:27 +1100

    Removed unused files and directories

--- env-setup-scripts/organizations-policies.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-# (c) Copyright 2022 Warwick Molloy, Kondensor
-# Published under GPLv3. See license.md
-
-echo "Listing all organization (sub account) service control policies"
-
-aws organizations list-policies --filter SERVICE_CONTROL_POLICY --output text
