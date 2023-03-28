3ed248e4871c9b1da107512dadd3339758e070ae
Warwick Molloy <wmolloy@gmail.com>
2023-03-29 08:23:11 +1100

    Added C# spec parser beginnings.

---
+++ policygen/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+ *  (c) Copyright 2022, 2023 Kondensor Contributors
+ *  Written by Warwick Molloy.
+ *  Distributed under the Kondensor License.
+ */
+
+
+StringReader line = new StringReader( "AbortMultipartUpload	Grants permission to abort a multipart upload	Write");
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Reading SPEC...");
+
+
+Spec.ReadStream(line);
---
+++ policygen/Spec.cs
@@ -0,0 +1,63 @@
+/*
+ *  (c) Copyright 2022, 2023 Kondensor Contributors
+ *  Written by Warwick Molloy.
+ *  Distributed under the Kondensor License.
+ */
+
+using Optional;
+
+using System.Text.RegularExpressions;
+using System.IO;
+
+public static class Spec
+{
+  private static readonly Regex Re = new Regex(
+    @"^(\w+)\s([\w \-]+)[\t]?([\w \-]+)[\t]?([\w \-]*)[\t]?",
+    RegexOptions.Compiled | RegexOptions.IgnoreCase
+  );
+
+  public static void ProcessDeclaration(string line)
+  {
+    var matches = Re.Matches(line);
+    //mm = re.search("^(\w+)\s([\w \-]+)[\t]?([\w \-]+)[\t]?([\w \-]*)[\t]?", line)
+    if (matches != null && matches.Count > 0)
+    {
+      GroupCollection groups = matches[0].Groups;
+      Console.WriteLine($"There are {groups.Count} matched groups.");
+      var action = groups[1].Value;
+      var description = groups[2].Value;
+      var access = groups[3].Value;
+        Console.WriteLine($"#0 -> '{groups[0]}'");
+      Console.WriteLine($"Action: {action}");
+      Console.WriteLine($"Desc: {description}");
+      Console.WriteLine($"Access: {access}");
+      for (int index = 4; index < groups.Count; index++)
+      {
+        Console.WriteLine($"#{index} -> '{groups[index]}'");
+      }
+      //nextLine = getLine()
+    // resource_types = getWord(nextLine)
+    // items = getList()
+    // (conditions, dependent_actions) = splitList(items, "iam")
+    // print("Action: %s" % action)
+    // print("Desc: %s" % description)
+    // print("Access: %s" % access)
+    // print("Res Types: %s" % resource_types)
+    // print("%d Conditions: %s" % (len(conditions),conditions) )
+    // print("%d Dependent Actions: %s" % (len(dependent_actions), dependent_actions))
+    // print("-----")
+
+    }
+  }
+
+  public static void ReadStream(TextReader reader)
+  {
+    var line = reader.ReadLine();
+    if (line != null)
+    {
+      ProcessDeclaration(line);
+    }
+    //
+  }
+
+}
