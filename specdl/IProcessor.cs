bc382dcaca5861dec77c5da102bd05eada78e2aa
Warwick Molloy <wmolloy@gmail.com>
2023-04-10 11:59:40 +1000

    WIP: processor idea generalised.

--- specdl/IProcessor.cs
+++ specdl/IProcessor.cs
@@ -6,10 +6,19 @@
 
 using System.IO;
 
+/// <summary>
+/// A processor reads each line of the input document, and optionally, writes
+/// to the output document. This interface allows processors to be plugged-in
+/// depending on context or situation.
+/// </summary>
 public interface IProcessor
 {
-  void SetInput(TextReader input);
-  void SetOutput(TextWriter output);
-
-  void ProcessAllLines(out int countHandled);
+  /// <summary>
+  /// Process all input lines of text. Write output lines as needed,
+  /// determined by the process method.
+  /// </summary>
+  /// <param name="countHandled">Number of lines processed.</param>
+  /// <param name="input">source of text to process.</param>
+  /// <param name="output">destination to write any processed data, if needed.</param>
+  void ProcessAllLines(out int countHandled, TextReader input, TextWriter output);
 }
---
+++ specdl/RootDocProcessor.cs
@@ -0,0 +1,11 @@
+/*
+ *  (c) Copyright 2022, 2023 Kondensor Contributors
+ *  Written by Warwick Molloy.
+ *  Distributed under the Kondensor License.
+ */
+
+
+public struct RootDocProcessor : IProcessor
+{
+  
+}
