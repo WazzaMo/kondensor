c3561eabdabd7e6a3b00077eeb527fa87d89bca9
Warwick Molloy <wmolloy@gmail.com>
2023-07-19 07:30:04 +1000

    RootDoc parsing and processing logic written.

--- specdl/RootDoc/RootDoc.cs
+++ specdl/RootDoc/RootDoc.cs
@@ -6,30 +6,57 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using Parser;
+using HtmlParse;
 
 namespace RootDoc;
 
 public struct RootDoc
 {
   private List<SubDoc> _Docs;
-  bool _IsAllMatched;
 
   public RootDoc()
   {
     _Docs = new List<SubDoc>();
-    _IsAllMatched = false;
   }
 
   public ParseAction ParseRootDoc(ParseAction parser)
   {
     parser
       .Expect(RootDocParser.RootDocProduction)
-      .AllMatchThen( (list, writer) => {
-        _IsAllMatched = true;
-      });
+      .AllMatchThen( SuccessfulParse );
       
     return parser;
   }
+
+  private void SuccessfulParse(LinkedList<Matching> matches, IPipeWriter writer)
+  {
+    CollectDocEntries(matches);
+  }
+
+  private void CollectDocEntries(LinkedList<Matching> matches)
+  {
+    IEnumerable<Matching> filtered = FilterNodes(matches);
+    Matching aHref;
+    string link, title;
+    SubDoc doc;
+
+    var entries = filtered.GetEnumerator();
+
+    while( entries.MoveNext() )
+    {
+      aHref = entries.Current;
+      link = HtmlPartsUtils.GetAHrefAttribValue(aHref.Parts);
+      title = HtmlPartsUtils.GetAHrefTagValue(aHref.Parts);
+      doc = new SubDoc(link, title);
+      _Docs.Add(doc);
+    }
+  }
+
+  private IEnumerable<Matching> FilterNodes(LinkedList<Matching> matches)
+    => from node in matches
+    where node.Annotation == RootAnnotations.START_AHREF
+    select node;
 }
