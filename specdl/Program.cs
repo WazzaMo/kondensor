67ed2d17afa8150ac1798b39ad4ab04d1c3e99ae
Warwick Molloy <wmolloy@gmail.com>
2023-07-29 08:37:15 +1000

    Program simplified and set to debug the new problem page - KafkaForMSK

--- specdl/Program.cs
+++ specdl/Program.cs
@@ -10,31 +10,10 @@ using System;
 
 
 
-// const string
-//   ALEXA_FOR_BUSINESS = "list_alexaforbusiness.html";
+const string
+  PROBLEM_PAGE = "list_apachekafkaapisforamazonmskclusters.html";
 
-//PageProcessingControl.ProcessSpecificPage(ALEXA_FOR_BUSINESS, Console.Out);
+PageProcessingControl.ProcessSpecificPage(PROBLEM_PAGE, Console.Out);
 
-PageProcessingControl.IterateThroughAllPagesAvailable();
+// PageProcessingControl.IterateThroughAllPagesAvailable();
 
-// SpecDownloader downloader = new SpecDownloader();
-// DocProcessor docProcessor = new DocProcessor();
-
-// downloader.SetDestination(Console.Out);
-// downloader.SetUrl(BASE_URL);
-// downloader.SetProcessor( docProcessor );
-
-// downloader.DownloadSource( ACCT_MGT_DOC );
-// downloader.Process(BASE_URL + "/" + ACCT_MGT_DOC);
-
-/*
-
-string currentDir = Directory.GetCurrentDirectory();
-
-Console.WriteLine(value: $"SPECDL running in: {currentDir}");
-
-DocumentIterator _DocIterator = new DocumentIterator();
-_DocIterator.LoadDocList();
-Console.WriteLine("Docs...");
-_DocIterator.IterateDocs();
-*/
