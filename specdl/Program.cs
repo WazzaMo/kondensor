b3835f0aee1f4587942bba2a069954daf2e99e11
Warwick Molloy <wmolloy@gmail.com>
2023-08-19 17:23:00 +1000

    Configured to download all specs.

--- specdl/Program.cs
+++ specdl/Program.cs
@@ -9,11 +9,11 @@
 using System;
 
 
-//'https://docs.aws.amazon.com/service-authorization/latest/reference/./list_awselasticbeanstalk.html'
+// 'https://docs.aws.amazon.com/service-authorization/latest/reference/./list_awselasticbeanstalk.html'
 
-const string
-  PROBLEM_PAGE = "list_awselasticbeanstalk.html";
-PageProcessingControl.ProcessSpecificPage(PROBLEM_PAGE, Console.Out);
+// const string
+//   PROBLEM_PAGE = "list_awselasticbeanstalk.html";
+// PageProcessingControl.ProcessSpecificPage(PROBLEM_PAGE, Console.Out);
 
-// PageProcessingControl.IterateThroughAllPagesAvailable();
+PageProcessingControl.IterateThroughAllPagesAvailable();
 
