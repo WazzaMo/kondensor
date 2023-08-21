67d814a1c923323f790d7ffb92c79c1aac79aa58
Warwick Molloy <wmolloy@gmail.com>
2023-08-22 08:35:28 +1000

    All pages parse successfully

--- specdl/Program.cs
+++ specdl/Program.cs
@@ -8,21 +8,9 @@
 
 using System;
 
+// const string
+//   PROBLEM_PAGE = "list_awsapplicationdiscoveryservice.html";
+// PageProcessingControl.ProcessSpecificPage(PROBLEM_PAGE, Console.Out);
 
-// 'https://docs.aws.amazon.com/service-authorization/latest/reference/list_amazonapigateway.html'
-
-// https://docs.aws.amazon.com/service-authorization/latest/reference/list_awsapplicationdiscoveryservice.html
-// https://docs.aws.amazon.com/service-authorization/latest/reference/list_amazoncloudwatchapplicationinsights.html
-// https://docs.aws.amazon.com/service-authorization/latest/reference/list_amazoncomprehendmedical.html
-// https://docs.aws.amazon.com/service-authorization/latest/reference/list_awscomputeoptimizer.html
-// https://docs.aws.amazon.com/service-authorization/latest/reference/list_awsmarketplace.html
-// https://docs.aws.amazon.com/service-authorization/latest/reference/list_amazonmessagedeliveryservice.html
-// https://docs.aws.amazon.com/service-authorization/latest/reference/list_awsopsworksconfigurationmanagement.html
-// https://docs.aws.amazon.com/service-authorization/latest/reference/list_amazonsessionmanagermessagegatewayservice.html
-
-const string
-  PROBLEM_PAGE = "list_awsapplicationdiscoveryservice.html";
-PageProcessingControl.ProcessSpecificPage(PROBLEM_PAGE, Console.Out);
-
-// return PageProcessingControl.IterateThroughAllPagesAvailable();
+return PageProcessingControl.IterateThroughAllPagesAvailable();
 
