9a5765579fd8aac4c31f44e4a59e8658bf09f7ad
Warwick Molloy <wmolloy@gmail.com>
2023-09-07 03:12:21 +1000

    Parsing is incomplete - resources and condition key tables do not parse. New lexing model likely needed.

--- specdl/Program.cs
+++ specdl/Program.cs
@@ -8,9 +8,11 @@
 
 using System;
 
+//  ENDLESS :https://docs.aws.amazon.com/service-authorization/latest/reference/list_amazonapigatewaymanagement.html
+
 // const string
-// 
-//   PROBLEM_PAGE = "list_amazonpersonalize.html";
+// 'https://docs.aws.amazon.com/service-authorization/latest/reference/./list_amazonapigateway.html'
+//   PROBLEM_PAGE = "./list_amazonapigateway.html";
 // PageProcessingControl.ProcessSpecificPage(PROBLEM_PAGE, Console.Out);
 
 return PageProcessingControl.IterateThroughAllPagesAvailable();
