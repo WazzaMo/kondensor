72d8142c66e424cf9d0c639ae8846914ff2acca6
Warwick Molloy <wmolloy@gmail.com>
2023-09-11 08:09:14 +1000

    Test cases for the TextPipeWriter and starting unit test code for fragmented HTML handling.

---
+++ test.kondensor.pipes/TestFragHtmlPipe.cs
@@ -0,0 +1,19 @@
+/*
+ *  (c) Copyright 2022, 2023 Kondensor Contributors
+ *  Written by Warwick Molloy.
+ *  Distributed without warranty, under the GNU Lesser Public License v 3.0
+ */
+
+using Xunit;
+
+using kondensor.Pipes;
+
+using System;
+using System.IO;
+
+namespace test.kondensor.pipes;
+
+public class TestFragHtmlPipe
+{
+  
+}
